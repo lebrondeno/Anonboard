00239b966b0beed179156b33ca354f59eca62abc
Lebrondeno <denochibo@gmail.com>
2026-04-02 12:58:49 +0300

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import Home from './pages/Home'
 import Submit from './pages/Submit'
 import Admin from './pages/Admin'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/s/:id" element={<Submit />} />
-        <Route path="/admin/:id" element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/s/:id" element={<Submit />} />
+          <Route path="/admin/:id" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
+      <Analytics />
+    </>
+  );
 }
